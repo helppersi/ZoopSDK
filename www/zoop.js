891ab4a54cbcd511b8eb2373abd9cc902570237a
Elian Medeiros <elian@cyanu.com>
2019-01-31 13:47:04 -0200

    teste de correção do nome

--- www/zoop.js
+++ www/zoop.js
@@ -1,5 +1,5 @@
 module.exports = {
 	startDiscovery: function(mensagem, sucesso, erro){
-		cordova.exec(sucesso, erro, "ZoopPlugin", "startDiscovery", []);
+		cordova.exec(sucesso, erro, "ZoopSDK", "startDiscovery", []);
 	}
 }
